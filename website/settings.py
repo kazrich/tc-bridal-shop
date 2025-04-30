21ea1f2dc5a60a606bfcb47101a3c8c9dd61f5b5
Kazrich <kazrichdyan2000@gmail.com>
2025-04-30 19:50:40 +0300

    image errors

--- website/settings.py
+++ website/settings.py
@@ -110,18 +110,13 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # only define once
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')    # for collectstatic
 
-STATIC_URL = 'static/'
-
-# For development, also add:
-STATICFILES_DIRS = [
-    BASE_DIR / "static",  # or os.path.join(BASE_DIR, 'static')
-]
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # required for collectstatic
-# Default primary key field type
+# Media files (uploaded images, user content)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
